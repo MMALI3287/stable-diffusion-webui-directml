2575f87537a24318a71524bf3c74864f574479cb
MD Musaddique Ali Erfan <erfanali3287@gmail.com>
2024-01-10 20:58:07 +0600

    new

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --backend directml --no-half --precision full --opt-sub-quad-attention --opt-split-attention-v1 --disable-nan-check --theme dark --autolaunch
+
+git pull
 
 call webui.bat
